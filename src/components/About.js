a09c9b4dce6e34e4bb098e8d8ced46a2b81d57d9
VANSHIKA SINGH <32262806+vanshikasingh0065@users.noreply.github.com>
2024-08-13 23:08:02 -0400

    update profile pic url

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
 
             <div className="three columns">
 
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+               <img className="profile-pic"  src="https://raw.githubusercontent.com/vanshikasingh0065/portfolio/main/public/images/profilepic.JPG" alt="" />
 
             </div>
 
