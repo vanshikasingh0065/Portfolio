47218b9bda522f622223340c4893b4ea29889e83
VANSHIKA SINGH <32262806+vanshikasingh0065@users.noreply.github.com>
2024-08-13 22:55:59 -0400

    demo 2

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
-export default class Porfolio extends Component {
+
+export default class Portfolio extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="portfolio">
-      <div className="row">
-        <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.</h1>
-          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-          {
-            resumeData.portfolio && resumeData.portfolio.map((item)=>{
-              return(
-                <div className="columns portfolio-item">
-                  <div className="item-wrap">
-                    <a href="#modal-01">
-                      <img src={`${item.imgurl}`} className="item-img"/>
-                      <div className="overlay">
-                        <div className="portfolio-item-meta">
-                          <h5>{item.name}</h5>
-                          <p>{item.description}</p>
-                        </div>
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check Out Some of My Works.</h1>
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+              {
+                resumeData.portfolio && resumeData.portfolio.map((item) => {
+                  return (
+                    <div className="columns portfolio-item" key={item.name}>
+                      <div className="item-wrap">
+                        <a href="#modal-01">
+                          <img src={`${item.imgurl}`} className="item-img" alt={item.name}/> {/* Added alt attribute */}
+                          <div className="overlay">
+                            <div className="portfolio-item-meta">
+                              <h5>{item.name}</h5>
+                              <p>{item.description}</p>
+                            </div>
+                          </div>
+                        </a>
                       </div>
-                    </a>
-                  </div>
-                </div>
-              )
-            })
-          }
+                    </div>
+                  )
+                })
+              }
+            </div>
           </div>
         </div>
-      </div>
-  </section>
-        );
+      </section>
+    );
   }
 }
